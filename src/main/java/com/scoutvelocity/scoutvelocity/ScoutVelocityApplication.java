81c8024bb6c74df50d4cdb6cdc7eacdf5e796749
leerura <sw09112@naver.com>
2026-01-13 19:27:48 +0900

    INIT: First Commit

---
+++ src/main/java/com/scoutvelocity/scoutvelocity/ScoutVelocityApplication.java
@@ -0,0 +1,13 @@
+package com.scoutvelocity.scoutvelocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutVelocityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoutVelocityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scoutvelocity/scoutvelocity/ScoutVelocityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoutvelocity.scoutvelocity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutVelocityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
